22cddf9b6e57e89b2703cb59c760839ba75ed642
Brock-hash <73358677+Brock-hash@users.noreply.github.com>
2025-12-23 15:27:58 +0100

    Update script.js

--- web-client/ch62/script.js
+++ web-client/ch62/script.js
@@ -1,1 +1,11 @@
-
+var getCookies = function(){
+  var pairs = document.cookie.split(";");
+  var cookies = {};
+  for (var i=0; i<pairs.length; i++){
+    var pair = pairs[i].split("=");
+    cookies[(pair[0]+'').trim()] = unescape(pair.slice(1).join('='));
+  }
+  return cookies;
+}
+var myCookies = getCookies();
+alert(myCookies.secret);
